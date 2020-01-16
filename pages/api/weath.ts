bd870c5ea0707bbb0911c42a8bd02fe614445ed0
Alex Odle <alex.odle@gmail.com>
2020-01-16 12:40:39 -0800

    rm tmp logging

--- pages/api/weath.ts
+++ pages/api/weath.ts
@@ -9,8 +9,6 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
     const files = await fsPromises.readdir(DIR)
     const strContents = await Promise.all(files.map(f => fsPromises.readFile(path.join(DIR, f))))
     const contents = strContents.map(s => JSON.parse(s as any as string))
-    console.log('hihi.apisending:')
-    console.log({ forecasts: contents })
     res.status(200).send({ forecasts: contents })
   } catch (e) {
     console.error(e.stack)
