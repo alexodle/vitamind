12b4be49643235439f962f81b981fbcf969f9f61
Alex Odle <alex.odle@gmail.com>
2020-02-01 16:22:16 -0800

    require env var at build-time

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-module.exports = {
-  env: {
-    // Redefining BASE_URL here causes it to get replaced with a string at build-time
-    BASE_URL: process.env.BASE_URL
+module.exports = () => {
+  if (!process.env.BASE_URL) {
+    throw new Error('Missing required env var at build-time')
+  }
+  return {
+    env: {
+      // Redefining BASE_URL here causes it to get replaced with a string at build-time
+      BASE_URL: process.env.BASE_URL
+    }
   }
 }
