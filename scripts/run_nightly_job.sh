8bf2143f4993bc60db5b250ca859356374b46d54
Alex Odle <alex.odle@gmail.com>
2020-03-05 17:06:25 +0000

    fix relative dir

--- scripts/run_nightly_job.sh
+++ scripts/run_nightly_job.sh
@@ -4,5 +4,7 @@ set -euf -o pipefail
 
 BASEDIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
+pushd $BASEDIR > /dev/null
 $BASEDIR/run_darksky_forecast_job.sh
 $BASEDIR/run_process_alerts_job.sh
+popd > /dev/null
